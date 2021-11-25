7018c4a3e06be78204d6e161094c14b667f5161c
Luiz Santana <luiz.santana>
2021-11-25 14:51:31 -0300

    Added configuration extension

---
+++ ConfigurationCustomExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration.CustomExtension
+{
+    /// <summary>
+    /// IConfiguration custom extension class
+    /// </summary>
+    public static class ConfigurationCustomExtension
+    {
+        /// <summary>
+        /// Get a custom value from appsettings.json file or EnvironmentVariables system from a given <see cref="string"/> object referencing to the custom value KEY
+        /// </summary>
+        /// <param name="configuration">The IConfiguration instance</param>
+        /// <param name="key">The KEY of the custom value</param>
+        /// <returns>The custom value string</returns>
+        public static string GetCustomValue(this IConfiguration configuration, string key)
+        {
+            return configuration[key] ?? configuration[key.ReverseKey()] ?? 
+                Environment.GetEnvironmentVariable(key) ?? Environment.GetEnvironmentVariable(key.ReverseKey());
+        }
+
+        private static string ReverseKey(this string key)
+        {
+            if (key.Contains("_"))
+                return key.Replace("_", ":");
+
+            return key.Replace(":", "_");
+        }
+    }
+}
